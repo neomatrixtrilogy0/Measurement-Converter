b3e59600416fcf544189320312a44d40860cf828
neo <neo@neo>
2021-02-27 22:55:13 -0500

    Added temperature measurement that converts F to C

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ document.getElementById('submit2').onclick = function() {
     let inches = document.getElementById('inch').value;
     document.getElementById('ft').innerHTML = inches / 12;
 }
+
+document.getElementById('submit3').onclick = function() {
+    let fahrenheit = document.getElementById('f').value;
+    document.getElementById('c').innerHTML = (fahrenheit-32)*(5/9);
+}
