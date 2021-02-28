b1ac3b31c1270012c04a94b5bb1a74c87ea9dab8
neo <neo@neo>
2021-02-27 20:07:11 -0500

    Added measurement to convert Inches to Feet

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ document.getElementById('submit').onclick = function() {
     let kg = document.getElementById('kg').value;
     document.getElementById('Lbs').innerHTML = kg * 2.205;
 }
+
+document.getElementById('submit2').onclick = function() {
+    let inches = document.getElementById('inch').value;
+    document.getElementById('ft').innerHTML = inches / 12;
+}
